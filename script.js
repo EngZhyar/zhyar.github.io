0989e490eb02a0ed4353bf18ac33f38b28ca1c60
EngZhyar <147944202+EngZhyar@users.noreply.github.com>
2024-06-27 15:38:01 +0300

    Update script.js

--- script.js
+++ script.js
@@ -15,6 +15,8 @@ document.addEventListener("DOMContentLoaded", function () {
 document.getElementById('callButton').addEventListener('click', function() {
   window.location.href = 'tel:+9647501238780';
 var whatsappLink="https://wa.me/+9647501238780?text="+ encodeURIComponent("Hi");
+  window.open(whatsappLink, '_blank');
+
 });
 
 
